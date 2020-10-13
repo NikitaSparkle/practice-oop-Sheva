221a4560cb090355a0c3c38167458db1d5f02a81
NikitaSparkle <sparta001sparta001@hotmail.com>
2020-10-13 18:32:53 +0300

    Task 1 and 2 are finished

--- src/main/java/org/itstep/task01/Main.java
+++ src/main/java/org/itstep/task01/Main.java
@@ -1,7 +1,5 @@
 package org.itstep.task01;
 
-import java.util.Scanner;
-
 /**
  * Задание 1
  * <p>
@@ -22,71 +20,3 @@ import java.util.Scanner;
  * <p>
  * Класс должен находиться в отдельном файле в этом же пакете
  */
-public class Main {
-    public static void main(String[] args) {
-        Human human1=new Human("Nick",14_04_2002,955625647,"Dnipro","Ukraine","Heroiv ave");
-        System.out.println("Name: "+human1.getFullName()+"\n");
-        System.out.println("Birthday: "+human1.getDateofBirth()+"\n");
-        System.out.println("Phone: +380"+human1.getPhone()+"\n");
-        System.out.println("City: "+human1.getCity()+"\n");
-        System.out.println("Country: "+human1.getCountry()+"\n");
-        System.out.println("Address: "+human1.getAddress()+"\n");
-
-    }
-}
-
-class Human {
-    private String fullName;
-    private int dateofBirth;
-    private int phone;
-    private String city;
-    private String country;
-    private String address;
-
-    Human() {
-        String fullName = "";
-        this.fullName = fullName;
-        dateofBirth = 00_00_0000;
-        phone = 0000000000;
-        String city = "";
-        this.city = city;
-        String country = "";
-        this.country = country;
-        String address = "";
-        this.address = address;
-    }
-
-    Human(String fullName,int dateofBirth,int phone,String city,String country,String address)
-    {
-        this.fullName=fullName;
-        this.dateofBirth=dateofBirth;
-        this.phone=phone;
-        this.city=city;
-        this.country=country;
-        this.address=address;
-    }
-
-    public String getFullName() { return fullName; }
-
-    public int getDateofBirth() { return dateofBirth; }
-
-    public int getPhone() { return phone; }
-
-    public String getCity() { return city; }
-
-    public String getCountry() { return country; }
-
-    public String getAddress() { return address; }
-
-    public void setFullName(String fullName) { this.fullName = fullName; }
-
-    public void setDateofBirth(int dateofBirth) { this.dateofBirth = dateofBirth; }
-
-    public void setPhone(int phone) { this.phone = phone; }
-
-    public void setCity(String city) { this.city = city; }
-
-    public void setCountry(String country) { this.country = country; }
-
-    public void setAddress(String address) { this.address = address; }
-}
